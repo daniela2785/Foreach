b0eda35b44637f99481fdebab3904727d6313049
daniela marques <dd978452@gmail.com>
2023-01-17 10:47:32 -0300

    Adicionar arquivos de projeto.

---
+++ Foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Foreach{
+
+	class Program{
+        static void Main(string[] args)
+		{
+			string[] palavras = { "Victor", "Lima", "Guia de programação", "Formação", "Node", "Curso de C#", "Udemy" };
+
+			foreach (string palavra in palavras)
+			{ // para cada (palavra no array palavras, repita o bloco de codigos)
+				Console.WriteLine(palavra);
+			}
+			Console.WriteLine("FIM DA LINHA");
+			Console.ReadLine();
+		}
+	}
+}
